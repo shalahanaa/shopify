62888b96cfaf9f7ed2264a60a355d5841bf5fb06
shalahanaa <shalahana07@gmail.com>
2023-04-12 21:26:04 +0200

    added navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,31 @@
+import "./Navbar.css";
+import {FaShopify} from "react-icons/fa";
+import {BsFillBrightnessHighFill} from "react-icons/bs";
+import {BiSearchAlt2} from "react-icons/bi";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="nav-left">
+          <ul className="nav-left-list">
+              <li className="logo"><FaShopify className="logo-icon"/>.dev</li>
+              <li>Solutions &#9660; </li>
+              <li>Blog</li>
+              <li>Documentation &#9660;</li>
+          </ul>
+      </div>
+      <div className="nav-right">
+        <ul className="nav-right-list">
+          <li><BiSearchAlt2 className="magnifier"/><input type="text" placeholder="Search docs" className="search"/>
+          <span className="slash">/</span>
+          </li>
+          <li><BsFillBrightnessHighFill className="brightness"/></li>
+          <li>Log in</li>
+          <li>Sign up</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
